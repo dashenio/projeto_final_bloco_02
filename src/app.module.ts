53d2b3b104f8fa00a3b9b28abe7d93264ff02d96
Vivian Rodrigues <zueleigafar@gmail.com>
2026-03-17 10:18:33 -0300

    📦 Configuration:  Configuração do Projeto e do Banco de Dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_farmacia',
+      entities: [],
+      synchronize: true,
+      logging: false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
