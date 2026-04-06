bec6c9d5ece87d307e157877c7d54ec0f9f2cf36
Vivian Rodrigues <zueleigafar@gmail.com>
2026-04-06 19:25:15 -0300

    🚀 Deploy:  Preparação para o deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaModule } from './categoria/categoria.module';
-import { Categoria } from './categoria/entities/catgoria.entity';
-import { Produto } from './produto/entities/produto.entity';
 import { ProdutoModule } from './produto/produto.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_farmacia',
-      entities: [Categoria, Produto, Usuario],
-      synchronize: true,
-      logging: false,
-    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+	    useClass: ProdService,
+      imports: [ConfigModule],
+}),
     CategoriaModule, ProdutoModule, UsuarioModule, AuthModule
   ],
   controllers: [AppController],
