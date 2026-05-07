f5e3b9cd6dfdf9bb306a3d2f8ef8b642b4e2e6ce
Vivian Rodrigues <zueleigafar@gmail.com>
2026-05-07 16:09:06 -0300

    🐛 FIX: Adequações de security

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -22,6 +22,9 @@ export class RolesGuard implements CanActivate {
 
     
     const { user } = context.switchToHttp().getRequest();
+
+    console.log('Dados do Usuário no Request:', user);
+
     return requiredRoles.some((role) => user.roles?.includes(role));
   }
 }
